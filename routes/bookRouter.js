2a518a76c818c6191ae639b5e080095a0686a34c
Nnamani Ugochukwu Frederick <royal.ugoh@gmail.com>
2022-07-23 20:34:33 +0100

    Implement http PUT, PATCH and DELETE verbs for /api/books/bookId

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -37,13 +37,13 @@ const routes = () => {
       });
     });
 
+  bookRouter.use('/:bookId', (req, res, next) => {
+    const { bookId } = req.params;
+    req.bookId = bookId;
+    next();
+  });
   bookRouter
     .route('/:bookId')
-    .all((req, res, next) => {
-      const { bookId } = req.params;
-      req.bookId = bookId;
-      next();
-    })
     .get((req, res) => {
       Book.findById(req.bookId, (err, foundBook) => {
         if (err) {
@@ -53,6 +53,37 @@ const routes = () => {
           });
         }
         return res.status(200).json(foundBook);
+      });
+    })
+    .put((req, res) => {
+      const { bookId } = req;
+      Book.replaceOne({ _id: bookId }, req.body, (err, replacedBook) => {
+        if (err) {
+          return res.status(404).json(err);
+        }
+        return res
+          .status(200)
+          .json({ message: 'Replace operation was successfull' });
+      });
+    })
+    .patch((req, res) => {
+      const { bookId } = req;
+      Book.updateOne({ _id: bookId }, req.body, (err, updatedBook) => {
+        if (err) {
+          return res.status(500).json(err);
+        }
+        return res
+          .status(200)
+          .json({ message: 'Update operation was successfull.' });
+      });
+    })
+    .delete((req, res) => {
+      const { bookId } = req;
+      Book.findByIdAndRemove(bookId, (err, deletedBook) => {
+        if (err) {
+          return res.status(500).json(err);
+        }
+        return res.sendStatus(204);
       });
     });
   return bookRouter;
